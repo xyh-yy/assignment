229f48c9e8f7f09684c8ea4c84c5f817af7c3803
1054013972@qq.com <1054013972@qq.com>
2025-03-16 19:31:40 +0800

    assignment4

---
+++ assignment4/HW1/HW1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HW1
+{
+    internal class Program
+    {
+        static double sum = 0;//当前所有数的和
+        static int num_cnt = 0;//当前已遍历的数的个数
+        static double average = 0;//均值
+        static double max = double.MinValue;//最大值
+        static double min = double.MaxValue;//最小值
+        static void Main(string[] args)
+        {
+            Action<double> action = delegate (double x)//计算平均值
+            {
+                sum += x;
+                num_cnt++;
+                average = sum/ num_cnt;
+            };
+
+            action += delegate (double x)//找出最大值
+            {
+                if (x > max) max = x;
+            };
+            action += delegate (double x)//找出最小值
+            {
+                if (x < min) min = x;
+            };
+
+            List<double> numbers = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            numbers.ForEach(action);
+            Console.WriteLine($"平均值是{average}");
+            Console.WriteLine($"最大值是{max}");
+            Console.WriteLine($"最小值是{min}");
+        }
+
+        
+    }
+}
---
+++ assignment4/HW2/HW2/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace HW2
+{
+    public class AlarmClock
+    {
+        // 委托
+        public delegate void TickHandler();
+        public delegate void AlarmHandler();
+
+        //事件
+        public event TickHandler Tick;     // 嘀嗒事件
+        public event AlarmHandler Alarm;   // 响铃事件
+
+        //时间
+        private int hour, minute, second;
+        private int alarmHour, alarmMinute ,alarmSecond;
+
+        //定义响铃时间
+        public AlarmClock(int alarmHour, int alarmMinute,int alarmSecond)
+        {
+            this.alarmHour = alarmHour;
+            this.alarmMinute = alarmMinute;
+            this.alarmSecond = alarmSecond;
+        }
+
+        // 每秒钟走一格，模拟时间的流逝
+        public void Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000); // 每秒走一次
+                second++;
+
+                if (second == 60)
+                {
+                    second = 0;
+                    minute++;
+                }
+
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+
+                if (hour == 24)
+                {
+                    hour = 0;
+                }
+
+                // 触发Tick事件
+                OnTick();
+
+                // 检查是否触发Alarm事件
+                if (hour == alarmHour && minute == alarmMinute && second == alarmSecond)
+                {
+                    OnAlarm();
+                    break;
+                }
+            }
+        }
+
+        // 触发Tick事件的方法
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke();
+        }
+
+        // 触发Alarm事件的方法
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke();
+        }
+    }
+
+    public class AlarmClockSubscriber
+    {
+        public void HandleTick()
+        {
+            Console.WriteLine("Tick");
+        }
+
+        public void HandleAlarm()
+        {
+            Console.WriteLine("Alarm!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock(0, 0, 5);
+            AlarmClockSubscriber subscriber = new AlarmClockSubscriber();
+
+            clock.Tick += subscriber.HandleTick;
+            clock.Alarm += subscriber.HandleAlarm;
+
+            clock.Start();
+        }
+    }
+}
